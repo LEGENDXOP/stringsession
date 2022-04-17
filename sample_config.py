cdb622dc26d0b8452802c5247d74bea8b4d062d4
LEGEND X <74249238+LEGENDXOP@users.noreply.github.com>
2022-04-17 15:47:11 +0530

    Update sample_config.py

--- sample_config.py
+++ sample_config.py
@@ -6,7 +6,7 @@ import os
 class Config(object):
     LOGGER = False
     # get a token from @BotFather
-    TG_BOT_TOKEN = os.environ.get('TG_BOT_TOKEN', None)
+    TG_BOT_TOKEN = "5050024245:AAGQDwE2TNRu2CwhcTiZuGPS53EJ2xluHF0"
     # required for running on Heroku
     URL = os.environ.get('URL', "")
     PORT = int(os.environ.get('PORT', 5000))
@@ -16,8 +16,8 @@ class Config(object):
     TG_DUMP_CHANNEL = int(os.environ.get("TG_DUMP_CHANNEL", "0"))
     #
     # Get this value from my.telegram.org! Please do not steal
-    APP_ID = int(os.environ.get("APP_ID", 6))
-    API_HASH = os.environ.get("API_HASH", "eb06d4abfb49dc3eeb1aeb98ae0f581e")
+    APP_ID = 16252641
+    API_HASH = "75fcc910aa8da30d41bf18f13865ba00"
     # the above example values will no longer work.
     # changed to List usage to circumvent https://t.me/UniBorg/56
 
